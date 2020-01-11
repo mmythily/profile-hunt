f798607463c97668a40335ee8bfec9bb5710c5f3
mmythily <mythily.mudunuru@gmail.com>
2020-01-11 14:56:58 -0500

    Updates

---
+++ precache-manifest.399b54c29d0edcd159aeedd7b0ff60b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca154653ea20dea02bec8a27fad65be",
+    "url": "/profile-hunt/index.html"
+  },
+  {
+    "revision": "65e8aea8498cbebcbee0",
+    "url": "/profile-hunt/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0dbb81f20b78201071b9",
+    "url": "/profile-hunt/static/css/main.1bb78f92.chunk.css"
+  },
+  {
+    "revision": "65e8aea8498cbebcbee0",
+    "url": "/profile-hunt/static/js/2.91541010.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile-hunt/static/js/2.91541010.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dbb81f20b78201071b9",
+    "url": "/profile-hunt/static/js/main.0dea72ec.chunk.js"
+  },
+  {
+    "revision": "4f6ab350ccd9162a9a50",
+    "url": "/profile-hunt/static/js/runtime-main.01c505e2.js"
+  }
+]);
